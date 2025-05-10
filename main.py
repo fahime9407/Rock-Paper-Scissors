321e455630cff1613e6bc2ba9ba47eea8180d9cd
fahime <196051112+fahime9407@users.noreply.github.com>
2025-05-10 23:15:04 +0330

    Update main.py

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ from tkinter import ttk
 
 ''' This function determines the winner between two items based on specific rules: 
     "kaghaz" beats "sang", "gheichi" beats "kaghaz", and "sang" beats "gheichi".
-    The function returns True if item_1 wins. '''
+    The function returns True if item_2 wins. '''
 def which_one(item_1, item_2):
     if item_1 != item_2 : # If the computer and the user have equal choices, no one wins.
         if item_1 == "sang" and item_2 == "gheichi":
-                return False
+            return False
         elif item_1 == "sang" and item_2 == "kaghaz":
-                return True
+            return True
         elif item_1 == "kaghaz" and item_2 == "sang":
             return False
         elif item_1 == "kaghaz" and item_2 == "gheichi":
-                return True
+            return True
         elif item_1 == "gheichi" and item_2 == "kaghaz":
             return False
         else :
@@ -23,61 +23,50 @@ def which_one(item_1, item_2):
 
 # This function is executed if the paper button is pressed.
 def paper():
-    global user_selection, pc_selection, flag, round_counter, pc_wins, user_wins
+    global user_selection, pc_selection
     user_selection = 'kaghaz'
-    flag = which_one(pc_selection, user_selection)
-    if flag:
-        user_wins += 1 # if the function return True, one is added to the user's points.
-    else: # Otherwise one is added to the computer's points.
-        pc_wins += 1
-    label_result.config(text= f"{pc_selection}")
-    if round_counter >= 3 : # The game is played in three rounds.
-        # At the end, whoever has the most points wins.
-        if user_wins > pc_wins :
-            label_result.config(text= f"you won!")
-        else :
-            label_result.config(text= f"you failed!")
-         
+    pc_selection = rnd.choice(options)
+    control()
 
 # This function is executed if the scissor button is pressed.
 def scissor():
-    global user_selection, pc_selection, flag, round_counter
+    global user_selection, pc_selection
     user_selection = 'gheichi'
-    flag = which_one(pc_selection, user_selection)
-    if flag:
-        user_wins += 1 # if the function return True, one is added to the user's points.
-    else: # Otherwise one is added to the computer's points.
-        pc_wins += 1
-    label_result.config(text= f"{pc_selection}")
-    if round_counter >= 3 : # The game is played in three rounds.
-        # At the end, whoever has the most points wins.
-        if user_wins > pc_wins :
-            label_result.config(text= f"you won!")
-        else :
-            label_result.config(text= f"you failed!")
+    pc_selection = rnd.choice(options)
+    control()
 
 # This function is executed if the stone button is pressed.
 def stone():
-    global user_selection, pc_selection, flag, round_counter
+    global user_selection, pc_selection
     user_selection = 'sang'
-    flag = which_one(pc_selection, user_selection)
-    if flag:
-        user_wins += 1 # if the function return True, one is added to the user's points.
-    else: # Otherwise one is added to the computer's points.
-        pc_wins += 1
-    label_result.config(text= f"{pc_selection}")
-    if round_counter >= 3 : # The game is played in three rounds.
-        # At the end, whoever has the most points wins.
-        if user_wins > pc_wins :
-            label_result.config(text= f"you won!")
-        else :
-            label_result.config(text= f"you failed!")
+    pc_selection = rnd.choice(options)
+    control()
 
-# The next two lines of code randomly selects an item from the list ["sang", "kaghaz", "gheichi"].
+def control():
+    global user_selection, pc_selection, flag, round_counter, pc_wins, user_wins
+    flag = which_one(pc_selection, user_selection)
+    if flag == True :
+        user_wins += 1 # if the function return True, it means the user won
+        round_counter += 1
+    elif flag == False : # if the function return False, it means the pc won
+        pc_wins += 1
+        round_counter += 1
+    else : # and if the function return nothing it means both of selections are same
+        pass
+    label_info.config(text= f"امتیاز تو : {user_wins} | امتیاز سیستم : {pc_wins} | دور بازی : {round_counter}")
+    label_result.config(text=f"pc selection : {pc_selection}")
+    if round_counter >= 3 :
+        if user_wins > pc_wins :
+            label_result.config(text= f"بردی")
+        else :
+            label_result.config(text= f"باختی")
+        round_counter, pc_wins, user_wins = 0, 0, 0 # start a new game after 3 round game
+
+
 options = ["sang", "kaghaz", "gheichi"]
-pc_selection = rnd.choice(options)
+pc_selection = ''
 user_selection = ''
-flag = False
+flag = 'nothing yet'
 pc_wins, user_wins = 0, 0
 round_counter = 0
 
@@ -89,6 +78,9 @@ label_1 = ttk.Label(frm, text="سنگ ، کاغذ یا قیچی ؟").grid(column
 ttk.Button(frm, text="کاغذ", command=paper).grid(column=1, row=1)
 ttk.Button(frm, text="قیچی", command=scissor).grid(column=1, row=2)
 ttk.Button(frm, text="سنگ", command=stone).grid(column=1, row=3)
-label_result = ttk.Label(frm, text="result : ")
-label_result.grid(column=0, row=4)
+label_info = ttk.Label(frm, text="امتیاز تو : 0 |امتیاز سیستم : 0 | دور بازی : 0")
+label_info.grid(column=0, row=4)
+label_result = ttk.Label(frm, text=f"pc selection : ")
+label_result.grid(column=0, row=2)
+ttk.Button(frm, text="پایان", command=root.destroy).grid(column=1, row=5)
 root.mainloop()
